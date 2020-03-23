74e4e5b80529f87587797506d84b7d189cce1192
Max2810 <maxime.fery@outlook.fr>
2020-03-23 15:16:36 +0100

    feat: align-center

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 "use strict";
+
+const wcwidth = require("@slimio/wcwidth");
+
+/**
+ * @function alignLeft
+ * @description Align string at left
+ * @param {string} str
+ * @param {number} width
+ * @returns {number}
+ */
+function alignLeft(str, width) {
+    const trimmed = str.trimRight();
+    if (trimmed.length === 0 && str.length >= width) {
+        return str;
+    }
+    let padding = "";
+    const strWidth = wcwidth(trimmed);
+
+    if (strWidth < width) {
+        padding = padding.padEnd(width - strWidth);
+    }
+
+    return trimmed + padding;
+}
+
+/**
+ * @function alignRight
+ * @description Align string at Right
+ * @param {string} str
+ * @param {number} width
+ * @returns {number}
+ */
+function alignRight(str, width) {
+    const trimmed = str.trimLeft();
+    if (trimmed.length === 0 && str.length >= width) {
+        return str;
+    }
+    let padding = "";
+    const strWidth = wcwidth(trimmed);
+
+    if (strWidth < width) {
+        padding = padding.padStart(width - strWidth);
+    }
+
+    return padding + trimmed;
+}
+
+/**
+ * @function alignCenter
+ * @description Align string at Center
+ * @param {string} str
+ * @param {number} width
+ * @returns {number}
+ */
+function alignCenter(str, width) {
+    const trimmed = str.trim();
+    if (trimmed.length === 0 && str.length >= width) {
+        return str;
+    }
+    let padLeft = "";
+    let padRight = "";
+    const strWidth = wcwidth(trimmed);
+
+    if (strWidth < width) {
+        const padLeftBy = parseInt((width - strWidth) / 2, 10);
+        padLeft = padLeft.padEnd(padLeftBy);
+        padRight = padRight.padStart(width - (strWidth + padLeftBy));
+    }
+
+    return padLeft + trimmed + padRight;
+}
+
+module.exports = {
+    alignCenter,
+    alignLeft,
+    alignRight
+};
--- test/test.js
+++ test/test.js
@@ -1,5 +1,111 @@
 "use strict";
 
-test("husky should pass test", () => {
-    expect(1).toStrictEqual(1);
+const { alignCenter, alignLeft, alignRight } = require("../index");
+
+
+test("center narrow", () => {
+    expect(alignCenter("abc", 10)).toBe("   abc    ");
+});
+
+test("center wide", () => {
+    expect(alignCenter("古古古", 10)).toBe("  古古古  ");
+});
+
+test("left narrow", () => {
+    expect(alignLeft("abc", 10)).toBe("abc       ");
+});
+
+test("left wide", () => {
+    expect(alignLeft("古古古", 10)).toBe("古古古    ");
+});
+
+test("right narrow", () => {
+    expect(alignRight("abc", 10)).toBe("       abc");
+});
+
+test("right narrow", () => {
+    expect(alignRight("古古古", 10)).toBe("    古古古");
+});
+
+// overflow
+
+test("center narrow overflow", () => {
+    expect(alignCenter("abc", 2)).toBe("abc");
+});
+
+test("center wide overflow", () => {
+    expect(alignCenter("古古古", 4)).toBe("古古古");
+});
+
+test("left narrow overflow", () => {
+    expect(alignLeft("abc", 2)).toBe("abc");
+});
+
+test("left wide overflow", () => {
+    expect(alignLeft("古古古", 4)).toBe("古古古");
+});
+
+test("right narrow overflow", () => {
+    expect(alignRight("abc", 2)).toBe("abc");
+});
+
+test("right narrow overflow", () => {
+    expect(alignRight("古古古", 4)).toBe("古古古");
+});
+
+
+// nothing
+
+test("left align nothing", () => {
+    expect(alignLeft("", 5)).toBe("     ");
+});
+
+test("center align nothing", () => {
+    expect(alignCenter("", 5)).toBe("     ");
+});
+
+test("right align nothing", () => {
+    expect(alignRight("", 5)).toBe("     ");
+});
+
+// whitespace
+
+test("left align whitespace", () => {
+    expect(alignLeft("   ", 5)).toBe("     ");
+});
+
+test("center align whitespace", () => {
+    expect(alignCenter("   ", 5)).toBe("     ");
+});
+
+test("right align whitespace", () => {
+    expect(alignRight("   ", 5)).toBe("     ");
+});
+
+// whitespace overflow
+
+test("left align whitespace overflow", () => {
+    expect(alignLeft("   ", 2)).toBe("   ");
+});
+
+test("center align whitespace overflow", () => {
+    expect(alignCenter("   ", 2)).toBe("   ");
+});
+
+test("right align whitespace overflow", () => {
+    expect(alignRight("   ", 2)).toBe("   ");
+});
+
+// whitespace mix
+
+test("left align whitespace mix", () => {
+    expect(alignLeft("  x  ", 10)).toBe("  x       ");
+});
+
+test("center align whitespace mix", () => {
+    expect(alignCenter("  x  ", 10)).toBe("    x     ");
+});
+
+test("right align whitespace mix", () => {
+    expect(alignRight("  x  ", 10)).toBe("       x  ");
 });
